83c4dc4b915896a4d882ab89cd24330e75127e98
Adesh Pandey <adesh.pandey10@gmail.com>
2020-10-13 21:47:09 +0530

    Update CreditCardInput.js
    depricated native module method was causing flickering issues

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -106,12 +106,12 @@ export default class CreditCardInput extends Component {
     const scrollResponder = this.refs.Form.getScrollResponder();
     const nodeHandle = ReactNative.findNodeHandle(this.refs[field]);
 
-    NativeModules.UIManager.measureLayoutRelativeToParent(nodeHandle,
-      e => { throw e; },
-      x => {
-        scrollResponder.scrollTo({ x: Math.max(x - PREVIOUS_FIELD_OFFSET, 0), animated: true });
-        this.refs[field].focus();
-      });
+//     NativeModules.UIManager.measureLayoutRelativeToParent(nodeHandle,
+//       e => { throw e; },
+//       x => {
+//         scrollResponder.scrollTo({ x: Math.max(x - PREVIOUS_FIELD_OFFSET, 0), animated: true });
+//         this.refs[field].focus();
+//       });
   }
 
   _inputProps = field => {
